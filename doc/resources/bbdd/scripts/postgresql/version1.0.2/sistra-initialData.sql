698f5563ee2495f8676a1d045779985a56b1a2fd
areus-ibit <areus-ibit@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2010-07-20 12:55:35 +0000

    Correcció dominis per postgresql
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/trunk@42 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- doc/resources/bbdd/scripts/postgresql/version1.0.2/sistra-initialData.sql
+++ doc/resources/bbdd/scripts/postgresql/version1.0.2/sistra-initialData.sql
@@ -29,25 +29,25 @@ SELECT pg_catalog.setval('str_seqtrv', 1, false);
 
 INSERT INTO str_orgres VALUES (1, 'GE-Genérico');
 
-INSERT INTO str_domin VALUES (1, 'GESACARBUA', 'Arbol unidadades administrativas', 'S', 'es.caib.sistra.db', 'select ''1'' codigo, null parent, ''Unidad Test'' descripcion from dual', 'L', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (2, 'GESACUADES', 'Descripción de unidad administrativa. Parametrizado por código unidad.', 'S', 'es.caib.sistra.db', 'SELECT  ''1'' CODIGO,''Unidad Test'' DESCRIPCION  FROM dual WHERE ''XX'' <> ?', 'L', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (3, 'GESACUNADM', 'Lista de unidades administrativas', 'S', 'es.caib.sistra.db', 'SELECT  ''1'' CODIGO,''Unidad Test'' DESCRIPCION  FROM dual', 'L', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (4, 'GERDSMODE', 'Lista de modelos de documentos del RDS', 'S', 'es.caib.redose.db', 'SELECT 	MOD_MODELO CODIGO,MOD_MODELO || '' - '' || MOD_NOMBRE DESCRIPCION FROM RDS_MODELO ORDER BY 1', 'L', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (5, 'GERDSVERS', 'Lista de versiones de un modelo de documento del RDS. Parametrizado por código modelo.', 'S', 'es.caib.redose.db', 'SELECT RDS_VERS.VER_VERSIO CODIGO, TO_CHAR(RDS_VERS.VER_VERSIO) DESCRIPCION FROM RDS_VERS,RDS_MODELO WHERE RDS_MODELO.MOD_CODIGO =  RDS_VERS.VER_CODMOD AND RDS_MODELO.MOD_MODELO = ? ORDER BY 1', 'L', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (6, 'GEFORMMODE', 'Lista de modelos de formularios', 'S', 'es.caib.rolforms.db', 'SELECT DISTINCT RFR_FORMUL.FOR_MODELO CODIGO,RFR_FORMUL.FOR_MODELO || '' - '' || RFR_TRAFOR.TRF_TITULO DESCRIPCION
+INSERT INTO str_domin VALUES (1, 'GESACARBUA', 'Arbol unidadades administrativas', 'S', 'es.caib.sistra.db', 'select ''1'' as "CODIGO", null as "PARENT", ''Unidad Test'' as "DESCRIPCION"', 'L', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (2, 'GESACUADES', 'Descripción de unidad administrativa. Parametrizado por código unidad.', 'S', 'es.caib.sistra.db', 'SELECT  ''1'' as "CODIGO", ''Unidad Test'' as "DESCRIPCION" WHERE ''XX'' <> ?', 'L', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (3, 'GESACUNADM', 'Lista de unidades administrativas', 'S', 'es.caib.sistra.db', 'SELECT  ''1'' as "CODIGO",''Unidad Test'' as "DESCRIPCION"', 'L', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (4, 'GERDSMODE', 'Lista de modelos de documentos del RDS', 'S', 'es.caib.redose.db', 'SELECT MOD_MODELO as "CODIGO", MOD_MODELO || '' - '' || MOD_NOMBRE as "DESCRIPCION" FROM RDS_MODELO ORDER BY 1', 'L', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (5, 'GERDSVERS', 'Lista de versiones de un modelo de documento del RDS. Parametrizado por código modelo.', 'S', 'es.caib.redose.db', 'SELECT RDS_VERS.VER_VERSIO as "CODIGO", cast(RDS_VERS.VER_VERSIO as text) as "DESCRIPCION" FROM RDS_VERS,RDS_MODELO WHERE RDS_MODELO.MOD_CODIGO =  RDS_VERS.VER_CODMOD AND RDS_MODELO.MOD_MODELO = ? ORDER BY 1', 'L', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (6, 'GEFORMMODE', 'Lista de modelos de formularios', 'S', 'es.caib.rolforms.db', 'SELECT DISTINCT RFR_FORMUL.FOR_MODELO as "CODIGO", RFR_FORMUL.FOR_MODELO || '' - '' || RFR_TRAFOR.TRF_TITULO as "DESCRIPCION"
 FROM RFR_FORMUL,RFR_TRAFOR
 WHERE RFR_TRAFOR.TRF_CODFOR = RFR_FORMUL.FOR_CODI AND RFR_TRAFOR.TRF_CODIDI = ''es''
-ORDER BY 1', 'R', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (7, 'GEFORMMOVE', 'Arbol de formularios y versiones', 'S', 'es.caib.rolforms.db', 'SELECT RFR_FORMUL.FOR_MODELO MODELO,
-	   RFR_FORMUL.FOR_MODELO || '' - '' || RFR_TRAFOR.TRF_TITULO DESCRIPCION,
-	   RFR_FORMUL.FOR_VERSIO VERSION	   	   
+ORDER BY 1', 'R', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (7, 'GEFORMMOVE', 'Arbol de formularios y versiones', 'S', 'es.caib.rolforms.db', 'SELECT RFR_FORMUL.FOR_MODELO as "MODELO",
+	   RFR_FORMUL.FOR_MODELO || '' - '' || RFR_TRAFOR.TRF_TITULO as "DESCRIPCION",
+	   RFR_FORMUL.FOR_VERSIO as "VERSION"
 FROM RFR_FORMUL,RFR_TRAFOR
 WHERE RFR_TRAFOR.TRF_CODFOR = RFR_FORMUL.FOR_CODI AND RFR_TRAFOR.TRF_CODIDI = ''es''
-ORDER BY 1,3', 'R', 'N', NULL, NULL, NULL, 'N', 1);
-INSERT INTO str_domin VALUES (8, 'GEFORMVERS', 'Lista de versiones de un formulario. Parametrizado por código de formulario.', 'S', 'es.caib.rolforms.db', 'SELECT  RFR_FORMUL.FOR_VERSIO CODIGO, RFR_FORMUL.FOR_VERSIO DESCRIPCION 
+ORDER BY 1,3', 'R', 'N', '', '', '', 'N', 1);
+INSERT INTO str_domin VALUES (8, 'GEFORMVERS', 'Lista de versiones de un formulario. Parametrizado por código de formulario.', 'S', 'es.caib.rolforms.db', 'SELECT  RFR_FORMUL.FOR_VERSIO as "CODIGO", RFR_FORMUL.FOR_VERSIO as "DESCRIPCION" 
 FROM RFR_FORMUL 
 WHERE RFR_FORMUL.FOR_MODELO = ?
-ORDER BY 1', 'R', 'N', NULL, NULL, NULL, 'N', 1);
+ORDER BY 1', 'R', 'N', '', '', '', 'N', 1);
 INSERT INTO str_domin VALUES (9, 'GEPAISES', 'Lista de paises', 'S', 'es.caib.sistra.db', 'SELECT TRIM( PAI_CODALF ) CODIGO, UPPER( PAI_DENCAS ) DESCRIPCION FROM STR_DPAIS WHERE PAI_VIGENC = ''S'' ORDER BY DESCRIPCION', 'L', 'N', NULL, NULL, NULL, 'N', 1);
 INSERT INTO str_domin VALUES (10, 'GEPROVINCI', 'Lista de provincias', 'S', 'es.caib.sistra.db', 'SELECT PRO_CODIGO CODIGO, PRO_DENCAT DESCRIPCION FROM STR_DPROVI ORDER BY CODIGO', 'L', 'N', NULL, NULL, NULL, 'N', 1);
 INSERT INTO str_domin VALUES (11, 'GEGMUNICI', 'Lista de municipios de una provincia. Parametrizado por código provincia.', 'S', 'es.caib.sistra.db', 'SELECT MUN_CODIGO CODIGO, MUN_DENOFI DESCRIPCION FROM STR_DMUNIC WHERE MUN_PROVIN = ? ORDER BY DESCRIPCION', 'L', 'N', NULL, NULL, NULL, 'N', 1);
