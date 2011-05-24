2ae324fe469bacc2756977f186bec805b8f4d5ec
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2011-05-24 08:33:37 +0000

    Separacion url fronts y backs (descarga @firma)
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/INDRA_103-110@110 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- moduls/modul-bantel/moduls/front/src/es/caib/bantel/front/controller/ExpedientesController.java
+++ moduls/modul-bantel/moduls/front/src/es/caib/bantel/front/controller/ExpedientesController.java
@@ -1,20 +1,13 @@
 package es.caib.bantel.front.controller;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts.tiles.ComponentContext;
 
-import es.caib.bantel.model.GestorBandeja;
-import es.caib.bantel.model.Tramite;
 import es.caib.bantel.persistence.delegate.ConfiguracionDelegate;
 import es.caib.bantel.persistence.delegate.DelegateUtil;
-import es.caib.bantel.persistence.delegate.GestorBandejaDelegate;
 
 public class ExpedientesController extends BaseController
 {
@@ -27,7 +20,7 @@ public class ExpedientesController extends BaseController
 		String urlSistra = "";
 		try{
 			ConfiguracionDelegate delegate = DelegateUtil.getConfiguracionDelegate();
-			urlSistra = delegate.obtenerConfiguracion().getProperty("sistra.url");
+			urlSistra = delegate.obtenerConfiguracion().getProperty("sistra.url.back");
 		}catch (Exception e) {
 			urlSistra = "";
 		}
