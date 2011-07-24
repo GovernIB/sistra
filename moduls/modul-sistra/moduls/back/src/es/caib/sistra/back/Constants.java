1c9fd0874bd8a552d367ac3964adb224acae50a5
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2011-07-24 09:06:16 +0000

    Verificacion de datos registros al importar
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/INDRA_103-110@207 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- moduls/modul-sistra/moduls/back/src/es/caib/sistra/back/Constants.java
+++ moduls/modul-sistra/moduls/back/src/es/caib/sistra/back/Constants.java
@@ -11,6 +11,15 @@ public class Constants implements Serializable
      * Atributo de contexto donde se guarda el lenguaje por defecto.
      */
     public static final String DEFAULT_LANG_KEY = "es.caib.sistra.back.DEFAULT_LANG";
-
+    
+    /**
+     * Lenguage por defecto
+     */
     public static final String DEFAULT_LANG 		= "ca";	
+    
+    
+    /**
+     * Atributo de sesion para almacenar el fichero xml de importacion que se esta tratando
+     */
+    public static final String XML_IMPORTACION_KEY 		= "es.caib.sistra.back.XML_IMPORTACION";
 }
--- moduls/modul-sistra/moduls/back/src/es/caib/sistra/back/action/xml/ImportarXMLPreviewAction.java
+++ moduls/modul-sistra/moduls/back/src/es/caib/sistra/back/action/xml/ImportarXMLPreviewAction.java
@@ -3,20 +3,22 @@ package es.caib.sistra.back.action.xml;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.betwixt.io.BeanReader;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.struts.action.ActionError;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import es.caib.regtel.persistence.delegate.DelegateRegtelUtil;
 import es.caib.regtel.persistence.delegate.RegistroTelematicoDelegate;
+import es.caib.sistra.back.Constants;
 import es.caib.sistra.back.action.BaseAction;
-import es.caib.sistra.back.action.menu.Nodo;
 import es.caib.sistra.back.form.ImportarVersionTramiteForm;
+import es.caib.sistra.back.form.ImportarVersionTramiteProcessForm;
 import es.caib.sistra.model.TramiteVersion;
 import es.caib.sistra.model.betwixt.Configurator;
 import es.caib.sistra.modelInterfaz.ConstantesDominio;
@@ -24,32 +26,29 @@ import es.caib.sistra.persistence.delegate.DelegateUtil;
 import es.caib.sistra.persistence.delegate.TramiteVersionDelegate;
 import es.caib.sistra.persistence.util.UtilDominios;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * @struts.action
  * name="importarVersionTramiteForm"
- * path="/importar/xml"
+ * path="/importar/xmlPreview"
  * scope="request"
  * validate="true"
  * input=".tramiteVersion.importar"
  *
  * @struts.action-forward
- *  name="success" path=".tramiteVersion.editar"
- *
+ *  name="success" path=".tramiteVersion.importarPreview"
+ *  					
  * @struts.action-forward
  *  name="fail" path=".tramiteVersion.importar"
  */
-public class ImportarXMLAction extends BaseAction 
+public class ImportarXMLPreviewAction extends BaseAction 
 {
     protected static final Log log = LogFactory.getLog(ImportarXMLAction.class);
 
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
                                  HttpServletResponse response) throws Exception {
+    	
+    	
         TramiteVersionDelegate delegate = DelegateUtil.getTramiteVersionDelegate();
-        boolean error = false;
-        ActionErrors messages = new ActionErrors();
         ImportarVersionTramiteForm iForm = (ImportarVersionTramiteForm) form;
         if (iForm.getFitxer() != null) {
 
@@ -59,31 +58,34 @@ public class ImportarXMLAction extends BaseAction
             TramiteVersion tramiteVersion = (TramiteVersion) beanReader.parse(iForm.getFitxer().getInputStream());
             tramiteVersion.setVersion( iForm.getVersion() );
             
+            // Almacenamos en sesion el objeto tramite version creado a partir del fichero de importacion
+            request.getSession().setAttribute(Constants.XML_IMPORTACION_KEY,tramiteVersion);
             
+            // Verificamos si existen los parametros de registro
             RegistroTelematicoDelegate dlgRte = DelegateRegtelUtil.getRegistroTelematicoDelegate();
-//            request.setAttribute( "listaunidadesadministrativas", ajustarTamListaDesplegable(  obtenerValoresDominio( ConstantesDominio.DOMINIO_SAC_UNIDADES_ADMINISTRATIVAS  ),  MAX_COMBO_DESC , "DESCRIPCION") );
-         
+            
+            String organoDestino = null;
             if(tramiteVersion.getOrganoDestino() != null){
-            	if(!dlgRte.existeServicioDestino(tramiteVersion.getOrganoDestino())){
-					messages.add(ActionErrors.GLOBAL_ERROR, new ActionError("error.organo.destino.no.existe"));
-            		error = true;
+            	if(dlgRte.existeServicioDestino(tramiteVersion.getOrganoDestino())){
+            		organoDestino = tramiteVersion.getOrganoDestino();
             	}
             }
             
+            String asunto = null;
             if(tramiteVersion.getRegistroAsunto() != null){
-            	if(!dlgRte.existeTipoAsunto(tramiteVersion.getRegistroAsunto())){
-            		messages.add(ActionErrors.GLOBAL_ERROR, new ActionError("error.tipo.asunto.no.existe"));
-            		error = true;
+            	if(dlgRte.existeTipoAsunto(tramiteVersion.getRegistroAsunto())){
+            		asunto = tramiteVersion.getRegistroAsunto();
             	}
             }
 			
+            String registroOficina = null;
 			if(tramiteVersion.getRegistroOficina() != null){
-				if(!dlgRte.existeOficinaRegistro(tramiteVersion.getRegistroOficina())){
-					messages.add(ActionErrors.GLOBAL_ERROR, new ActionError("error.oficina.registro.no.existe"));
-            		error = true;
+				if(dlgRte.existeOficinaRegistro(tramiteVersion.getRegistroOficina())){
+					registroOficina = tramiteVersion.getRegistroOficina();
             	}
 			}
 			
+			Long unidadAdministrativa=null;
 			if(tramiteVersion.getUnidadAdministrativa() != null){
 				boolean exist = false;
 				List unidades = UtilDominios.obtenerValoresDominio( ConstantesDominio.DOMINIO_SAC_UNIDADES_ADMINISTRATIVAS, null );
@@ -93,26 +95,23 @@ public class ImportarXMLAction extends BaseAction
 	    				exist = true;
 	    			}
 				}
-				if(!exist){
-					messages.add(ActionErrors.GLOBAL_ERROR, new ActionError("error.unidad.administrativa.no.existe"));
-            		error = true;
-				}
+				if(exist){
+					unidadAdministrativa = tramiteVersion.getUnidadAdministrativa();
+            	}
             }
-			if(error){
-				saveErrors(request,messages);  
-				return mapping.findForward("fail");
-			}else{
-	            log.info("Gravant formulari: " + tramiteVersion.getVersion() );
-	            Long idTramiteVersion 	= delegate.grabarTramiteVersion( tramiteVersion, iForm.getCodigoTramite() );
-	
-	            guardarTramiteVersion(mapping, request, idTramiteVersion );
-	            
-	            this.setReloadTree( request, Nodo.IR_A_DEFINICION_TRAMITE_VERSION, idTramiteVersion );
-	
-	            return mapping.findForward("success");
-	        }
+			
+			// Pasamos a pantalla para cambiar parametros registro
+			ImportarVersionTramiteProcessForm iFormProcess = (ImportarVersionTramiteProcessForm) obtenerActionForm(mapping,request,"/importar/xmlProcess");
+			iFormProcess.setCodigoTramite(iForm.getCodigoTramite());
+			iFormProcess.setUnidadAdministrativa(unidadAdministrativa);
+			iFormProcess.setRegistroOficina(registroOficina);
+			iFormProcess.setOrganoDestino(organoDestino);
+			iFormProcess.setRegistroAsunto(asunto);
+			return mapping.findForward("success");
+			
+        }else{
+        	return mapping.findForward("fail");	
         }
-
-        return mapping.findForward("fail");
+        
     }
 }
