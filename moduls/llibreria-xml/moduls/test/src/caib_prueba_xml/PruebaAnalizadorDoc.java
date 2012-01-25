e548b30d57bfe573702260dc5f453bd7d2618585
ihdelpino-indra <ihdelpino-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-01-25 15:43:37 +0000

    mid: 0000091 msg: Nova utilitat per a extreure dades d'un formulari basat en un docx
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/INDRA_103-110@332 5878cf19-a0df-4652-bd9f-a806d42fd1e1

---
+++ moduls/llibreria-xml/moduls/test/src/caib_prueba_xml/PruebaAnalizadorDoc.java
@@ -0,0 +1,77 @@
+package caib_prueba_xml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+
+import es.caib.xml.ConstantesXML;
+import es.caib.xml.analiza.Generador;
+import es.caib.xml.analiza.Nodo;
+import es.caib.xml.analiza.formdoc.AnalizadorDoc;
+import es.caib.xml.analiza.formdoc.AnalizadorDocException;
+import es.caib.xml.util.HashMapIterable;
+
+/**
+ * Clase para probar el funcionamiento de AnalizadorDoc
+ * @author ihdelpino
+ *
+ */
+public class PruebaAnalizadorDoc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String inputFilename = args[0];
+		try {
+			System.out.println("Inicio prueba");
+			String[] aux = inputFilename.split("\\.");
+			System.out.println("Fichero: " + inputFilename);
+			System.out.println("Tipo fichero: " + aux[1]);
+			FileInputStream contenido = new FileInputStream(inputFilename);
+			AnalizadorDoc analizador = new AnalizadorDoc();
+			HashMapIterable resultado = analizador.analizar(contenido, aux[1]);
+			imprimirResultado(resultado);
+			String xml = getXML(resultado);
+			System.out.println("XML: ");
+			System.out.println(xml);
+			System.out.println("Fin prueba");
+		} catch (FileNotFoundException e) {
+			System.out.println("El fichero " + inputFilename + " no existe.");
+		} catch (AnalizadorDocException a) {
+			System.out.println(a.getMessage());
+		}
+
+	}
+	
+	/**
+	 * Muestra el contenido del análisis
+	 * @param resultado
+	 */
+	private static void imprimirResultado(HashMapIterable resultado) {
+		Iterator it = resultado.iterator();
+		while (it.hasNext()) {
+			Nodo n = (Nodo)it.next();
+			System.out.println("XPath: " + n.getXpath());
+			System.out.println("Valor: " + n.getValor());
+		}
+	}
+	
+	/**
+	 * Obtiene un XML a partir de los datos del documento
+	 * @param resultado
+	 * @return
+	 */
+	private static String getXML(HashMapIterable resultado) {
+		try {
+			Generador generador = new Generador ();
+			generador.setEncoding(ConstantesXML.ENCODING);
+			return generador.generarXML(resultado);
+		} catch (Exception e) {
+			//TODO
+			return "Eror al generar el XML : " + e.getMessage();
+		}
+
+	}
+
+}
--- moduls/llibreria-xml/moduls/xml/src/es/caib/xml/analiza/formdoc/AnalizadorDoc.java
+++ moduls/llibreria-xml/moduls/xml/src/es/caib/xml/analiza/formdoc/AnalizadorDoc.java
@@ -1,6 +1,27 @@
 package es.caib.xml.analiza.formdoc;
 
 import java.io.InputStream;
+import java.util.List;
+
+import javax.xml.bind.Binder;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+
+import org.docx4j.XmlUtils;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.BooleanDefaultTrue;
+import org.docx4j.wml.CTFFCheckBox;
+import org.docx4j.wml.CTFFDDList;
+import org.docx4j.wml.CTFFDDList.ListEntry;
+import org.docx4j.wml.CTFFDDList.Result;
+import org.docx4j.wml.CTFFData;
+import org.docx4j.wml.CTFFName;
+import org.docx4j.wml.CTFFTextInput;
+import org.docx4j.wml.P;
+import org.docx4j.wml.Text;
+import org.w3c.dom.Node;
 
 import es.caib.xml.analiza.Nodo;
 import es.caib.xml.util.HashMapIterable;
@@ -20,29 +41,89 @@ public class AnalizadorDoc {
 	 */
 	public HashMapIterable analizar(InputStream contenido, String tipoDocumento) throws AnalizadorDocException {
 	
-		// TODO IMPLEMENTAR
-	
 		// Control tipos validos
-		if ("docx".equals(tipoDocumento)) {
+		//de momento solo aceptamos docx
+		if (!"docx".equals(tipoDocumento)) {
 			throw new AnalizadorDocException("El tipo de documento " + tipoDocumento + " no es valido");
 		}
 		
 		// Interpretar documento y devolver xpaths
 		try {
-			Nodo nodo1 = new Nodo("/FORMULARIO/CAMPO1", "VALOR CAMPO 1");
-			Nodo nodo2 = new Nodo("/FORMULARIO/CAMPO2", "VALOR CAMPO 2");
-			Nodo nodo3 = new Nodo("/FORMULARIO/CAMPO3", "VALOR CAMPO 3");
-					
-			HashMapIterable hojas = new HashMapIterable();
-			hojas.put(nodo1.getXpath(),nodo1);
-			hojas.put(nodo2.getXpath(),nodo2);
-			hojas.put(nodo3.getXpath(),nodo3);
 			
+			WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(contenido);
+			MainDocumentPart main = wordMLPackage.getMainDocumentPart();
+			List<Object> lstParagraphs = main.getJAXBNodesViaXPath("//w:p", false);
+			Binder<Node> binder = main.getBinder();
+			HashMapIterable hojas = new HashMapIterable();
+			String xpath = null;
+			String nombre;
+			P elem = null;
+			//Recorremos los paragrafos
+			for (int i = 0; i < lstParagraphs.size(); i++) {
+
+				elem = (P)lstParagraphs.get(i);
+				//Para cada paragrafo buscamos el ffData de inicio para obtener el nombre de los campos que hay y el tipo
+				List<Object> lstFfdata = XmlUtils.getJAXBNodesViaXPath(binder, elem, ".//w:ffData", false);
+				//prerequisito solo un ffData por paragrafo
+				if (!lstFfdata.isEmpty()) {
+					CTFFData data = (CTFFData)lstFfdata.get(0);
+					List<JAXBElement<?>> lstNecoe = data.getNameOrEnabledOrCalcOnExit();
+					JAXBElement<?> necoe = null;
+					for (int e = 0; e < lstNecoe.size(); e++) {
+						necoe = lstNecoe.get(e);
+						//nombre
+						if (necoe.getValue() instanceof CTFFName) {
+							nombre = ((CTFFName)necoe.getValue()).getVal();
+							//sustituimos el . por /
+							nombre = nombre.replace('_', '/');
+							xpath = "/FORMULARIO/" + nombre;
+							//System.out.println("Nombre campo: " + nom);
+						} else if (necoe.getValue() instanceof CTFFTextInput) {
+							//si es textinput debemos buscar un elemento de texto
+							List<Object> lstText = XmlUtils.getJAXBNodesViaXPath(binder, elem, ".//w:t[not(@xml:space)]", false);
+							if (!lstText.isEmpty()) {
+								JAXBElement<?> telem = (JAXBElement<?>)lstText.get(0);
+								String valor = ((Text)telem.getValue()).getValue(); 
+								Nodo nodo = new Nodo(xpath, valor);
+								hojas.put(nodo.getXpath(), nodo);
+							}
+						} else if (necoe.getValue() instanceof CTFFCheckBox) {
+							//checkbox
+							BooleanDefaultTrue bValor = ((CTFFCheckBox)necoe.getValue()).getChecked();
+							String valor;
+							if (bValor == null || !bValor.isVal()) valor = "false";
+							else valor = "true";
+							
+							Nodo nodo = new Nodo(xpath, valor);
+							hojas.put(nodo.getXpath(), nodo);
+						} else if (necoe.getValue() instanceof CTFFDDList) {
+							//lista desplegable
+							CTFFDDList desplegable = (CTFFDDList)necoe.getValue(); 
+							Result r = desplegable.getResult();
+							String valor = null;
+							if (r != null) {//Confirmamos que se selecciona valor
+								ListEntry seleccionado = desplegable.getListEntry().get(r.getVal().intValue());
+								valor = seleccionado.getVal();
+							}
+							Nodo nodo = new Nodo(xpath, valor);
+							hojas.put(nodo.getXpath(), nodo);
+						}
+						
+					}	
+				}
+			}
+
 			return hojas;
+		} catch (Docx4JException d) {
+			throw new AnalizadorDocException("Se ha producido una excepcion al cargar el documento: " + d.getMessage(), d);
+		} catch (JAXBException j) {
+			throw new AnalizadorDocException("Se ha producido una excepcion al obtener algun nodo del documento: " + j.getMessage(), j);
 		} catch (Exception ex) {
+			ex.printStackTrace();
 			throw new AnalizadorDocException("Se ha producido una excepcion al interpretar documento: " + ex.getMessage(), ex);
 		}
 		
 	}
-	
 }
+
+
