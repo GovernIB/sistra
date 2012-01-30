5f30cd29dfab639a7d9ff018963e89c26e031341
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-01-30 09:20:09 +0000

    mid: 0000025 msg: Adjunta al justificante de registro estandar los formularios principales
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/INDRA_103-110@336 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- doc/resources/bbdd/scripts/oracle/version1.1.5/sistra-update.sql
+++ doc/resources/bbdd/scripts/oracle/version1.1.5/sistra-update.sql
@@ -1,3 +1,3 @@
-alter table STR_DOCUM  add DOC_FORJUS VARCHAR2(1) default 'N';
+alter table STR_DOCUM  add DOC_FORAJU VARCHAR2(1) default 'N';
 comment on column STR_DOCUM.DOC_FORAJU is
 'Para Formulario: se indica si el formulario debe anexarse al justificante';
