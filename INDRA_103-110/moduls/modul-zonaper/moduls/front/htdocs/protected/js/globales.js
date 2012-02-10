dcfbcfccd590a63b4c16144e0884cefee7564aa3
ihdelpino-indra <ihdelpino-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-02-10 13:01:19 +0000

    mid: 0000095 msg: En la versión 10.0 de Firefox se muestra el aviso que la versión ha de ser superior a la 1.5, erroneamente Se modifican los JavaScript afectados en sistrafornt y zonaperfront.
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@349 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- INDRA_103-110/moduls/modul-zonaper/moduls/front/htdocs/protected/js/globales.js
+++ INDRA_103-110/moduls/modul-zonaper/moduls/front/htdocs/protected/js/globales.js
@@ -137,16 +137,19 @@ function checkVersionNavegador(errorIE,errorFirefox){
 	}
 	
 	if (browser != "") {
-		version = detect.charAt(place + thestring.length);
-		posDecimal = place + thestring.length + 1;
-		if (detect.charAt(posDecimal) == '.'){	
-			do{
-			  version = version + '' + detect.charAt(posDecimal);		
-			  posDecimal++;								
-			}while (!isNaN(detect.charAt(posDecimal) )&& posDecimal < detect.length);		
-		}
+		//obtenemos el substring donde empieza la versión
+		aux = detect.substring(place + thestring.length);
+		//buscamos donde esta el separador decimal
+		posDecimal = aux.indexOf('.');
+		//obtenemos la parte entera
+		version = aux.substring(0, posDecimal);
+		//obtenemos la parte decimal
+		do{
+		  version = version + '' + aux.charAt(posDecimal);		
+		  posDecimal++;
+		}while (!isNaN(aux.charAt(posDecimal)) && posDecimal < aux.length);		
 	}
-	
+		
 	if (browser == "Internet Explorer" && parseFloat( version, 10) < 6 ){
 		if (readCookie("avisoVersionExplorador") == null){
 			createCookie("avisoVersionExplorador","avisado",0);
