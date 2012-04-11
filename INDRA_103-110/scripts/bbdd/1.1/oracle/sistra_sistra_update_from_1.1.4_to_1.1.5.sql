bb1b93c84e667752a30c772865f7118d2a1a8296
ihdelpino-indra <ihdelpino-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-04-11 08:33:12 +0000

    mid: 0000110 msg: Cambio de tamaño del código de formulario en FORMS
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@385 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- INDRA_103-110/scripts/bbdd/1.1/oracle/sistra_sistra_update_from_1.1.4_to_1.1.5.sql
+++ INDRA_103-110/scripts/bbdd/1.1/oracle/sistra_sistra_update_from_1.1.4_to_1.1.5.sql
@@ -1,3 +1,5 @@
 alter table STR_DOCUM  add DOC_FORAJU VARCHAR2(1) default 'N';
 comment on column STR_DOCUM.DOC_FORAJU is
 'Para Formulario: se indica si el formulario debe anexarse al justificante';
+
+alter table STR_DOCNIV modify DNV_FORFOR VARCHAR2(20 char);
