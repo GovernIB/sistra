04e437eccd9292a35e4103a1a699d10349656825
anadal-ibit <anadal-ibit@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-09-11 06:43:45 +0000

    mid: 0000152 msg: Tasca Continua d'Actualització d'Scripts de BBDD (Faltaven usuaris)
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/INDRA_103-110@468 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- scripts/bbdd/1.1/oracle/create/sistra_loginMock_create_data.sql
+++ scripts/bbdd/1.1/oracle/create/sistra_loginMock_create_data.sql
@@ -56,17 +56,3 @@ INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES (
 
 INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
 VALUES ( 'nobody', 'nobody' , 'Usuari No Autenticat' , '00000000T' );
-
-
-COMMIT;
-
-
-
-
-
-
-
-
-	
-
-
