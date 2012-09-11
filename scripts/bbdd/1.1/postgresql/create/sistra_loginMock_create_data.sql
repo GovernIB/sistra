21869cd97eba17890e47a6523bdcc207252e7599
anadal-ibit <anadal-ibit@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-09-11 07:42:05 +0000

    mid: 0000152 msg: Tasca Continua d'Actualització d'Scripts de BBDD (Conflictes de NIF duplicat i per postgresql faltaven usuaris)
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/INDRA_103-110@469 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- scripts/bbdd/1.1/postgresql/create/sistra_loginMock_create_data.sql
+++ scripts/bbdd/1.1/postgresql/create/sistra_loginMock_create_data.sql
@@ -1,5 +1,30 @@
-﻿INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES ( 
-'admin', 'tothom'); 
+﻿INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'admin', 'admin' , 'Usuario administrador' ,null );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'gestor', 'gestor' , 'Usuario gestor' ,null );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'registro', 'registro' , 'Usuario punto registro' ,null );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'helpdesk', 'helpdesk' , 'Usuario HelpDesk' ,null );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'auto', 'auto' , 'Usuario procesos automaticos' ,null );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'demo', 'demo' , 'Jose García García' ,'12345678Z' );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'delega', 'delega' , 'Administrador representaciones' ,null );
+
+INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
+VALUES ( 'nobody', 'nobody' , 'Usuari No Autenticat' , '00000000T' );
+
+INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES ( 
+'admin', 'tothom');
+
 INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES ( 
 'admin', 'STR_ADMIN'); 
 
@@ -25,16 +50,10 @@ INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES (
 'demo', 'tothom'); 
 
 INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES ( 
-'nobody', 'tothom'); 
-
-INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
-VALUES ( 'delega', 'delega' , 'Administrador representaciones' ,null );
+'nobody', 'tothom');
 
 INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES ( 
 'delega', 'STR_DELEGA'); 
 
 INSERT INTO SC_WL_USUGRU ( UGR_CODUSU, UGR_CODGRU ) VALUES ( 
 'delega', 'tothom'); 
-
-INSERT INTO SC_WL_USUARI (USU_CODI, USU_PASS, USU_NOM, USU_NIF) 
-VALUES ( 'nobody', 'nobody' , 'Usuari No Autenticat' , '00000000T' );
