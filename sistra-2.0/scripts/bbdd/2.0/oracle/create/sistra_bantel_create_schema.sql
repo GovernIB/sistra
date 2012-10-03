4354766ed2159bc0ffa9747e4cc0a61641602fce
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2012-10-03 14:33:56 +0000

    mid: 0000169 msg: Recreacion indices Bantel (FK a procedimientos)
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@488 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- sistra-2.0/scripts/bbdd/2.0/oracle/create/sistra_bantel_create_schema.sql
+++ sistra-2.0/scripts/bbdd/2.0/oracle/create/sistra_bantel_create_schema.sql
@@ -399,8 +399,12 @@ alter table BTE_TRAMIT
 alter table BTE_TRAMIT
    add constraint BTE_NUMPRE_UNI unique (TRA_NUMPRE);
 
-create index BTE_TRATAP_FK_I on BTE_TRAMIT (
+create index BTE_TRATRA_I on BTE_TRAMIT (
    TRA_IDETRA ASC
+);
+
+create index BTE_TRATAP_FK_I on BTE_TRAMIT (
+   TRA_IDEPRO ASC
 );
 
 alter table BTE_TRAMIT add constraint BTE_TRATAP_FK foreign key (TRA_IDEPRO)
