bab4bcbdadd1e70d690e03fe6c8c6b052595adf0
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2013-02-15 11:56:43 +0000

    mid: 0000236 msg: Dominios mantenidos por gestores desde Bantelfront
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-2.2@639 5878cf19-a0df-4652-bd9f-a806d42fd1e1

---
+++ scripts/bbdd/2.2/oracle/update_from_2.1/sistra_bantel_update_from_2.1.sql
@@ -0,0 +1,148 @@
+create sequence BTE_SEQCFU;
+
+create sequence BTE_SEQFIF;
+
+create sequence BTE_SEQFUE;
+
+create sequence BTE_SEQVCF;
+
+create table BTE_CAMFUE  (
+   CFU_CODIGO           NUMBER(20)                      not null,
+   CFU_CODFUE           NUMBER(20)                      not null,
+   CFU_IDENT            VARCHAR2(20)                    not null
+);
+
+comment on table BTE_CAMFUE is
+'Definicion campos fuente datos';
+
+comment on column BTE_CAMFUE.CFU_CODIGO is
+'Codigo interno';
+
+comment on column BTE_CAMFUE.CFU_CODFUE is
+'Codigo interno fuente';
+
+comment on column BTE_CAMFUE.CFU_IDENT is
+'Id campo';
+
+
+alter table BTE_CAMFUE
+   add constraint BTE_CFU_PK primary key (CFU_CODIGO);
+
+create unique index BTE_CFUIDENT_AK on BTE_CAMFUE (
+   CFU_CODFUE ASC,
+   CFU_IDENT ASC
+);
+
+create index BTE_CODFUE_I on BTE_CAMFUE (
+   CFU_CODFUE ASC
+);
+
+
+create table BTE_FILFUE  (
+   FIF_CODIGO           NUMBER(20)                      not null,
+   FIF_CODFUE           NUMBER(20)                      not null
+);
+
+comment on table BTE_FILFUE is
+'Filas de datos fuente datos';
+
+comment on column BTE_FILFUE.FIF_CODIGO is
+'Codigo interno';
+
+comment on column BTE_FILFUE.FIF_CODFUE is
+'Codigo interno fuente datos';
+
+alter table BTE_FILFUE
+   add constraint BTE_FIF_PK primary key (FIF_CODIGO);
+
+create index BTE_FIFFUE_I on BTE_FILFUE (
+   FIF_CODFUE ASC
+);
+
+create table BTE_FUEDAT  (
+   FUE_CODIGO           NUMBER(20)                      not null,
+   FUE_IDENT            VARCHAR2(20)                    not null,
+   FUE_DESC             VARCHAR2(500),
+   FUE_IDEPRO           VARCHAR2(100)                   not null
+);
+
+comment on table BTE_FUEDAT is
+'Fuentes de datos para dominios';
+
+comment on column BTE_FUEDAT.FUE_CODIGO is
+'Codigo interno';
+
+comment on column BTE_FUEDAT.FUE_IDENT is
+'Identificador fuente datos';
+
+comment on column BTE_FUEDAT.FUE_DESC is
+'Descripcion';
+
+comment on column BTE_FUEDAT.FUE_IDEPRO is
+'IDENTIFICADOR DEL PROCEDIMIENTO';
+
+alter table BTE_FUEDAT
+   add constraint BTE_FUE_PK primary key (FUE_CODIGO);
+
+create unique index BTE_FUEIDE_AK on BTE_FUEDAT (
+   FUE_IDENT ASC
+);
+
+create index BTE_FUEIDP_I on BTE_FUEDAT (
+   FUE_IDEPRO ASC
+);
+
+
+create table BTE_VALCFU  (
+   VCF_CODIGO           NUMBER(20)                      not null,
+   VCF_CODFIF          NUMBER(20)                      not null,
+   VCF_CODCFU           NUMBER(20)                      not null,
+   VCF_VALOR            VARCHAR2(4000)
+);
+
+comment on table BTE_VALCFU is
+'Valores fuente datos';
+
+comment on column BTE_VALCFU.VCF_CODIGO is
+'Codigo interno';
+
+comment on column BTE_VALCFU.VCF_CODFIFU is
+'Codigo interno fila fuente datos';
+
+comment on column BTE_VALCFU.VCF_CODCFU is
+'Codigo interno campo fuente datos';
+
+comment on column BTE_VALCFU.VCF_VALOR is
+'Valor';
+
+alter table BTE_VALCFU
+   add constraint BTE_VCF_PK primary key (VCF_CODIGO);
+
+create index BTE_VCFCFU_I on BTE_VALCFU (
+   VCF_CODCFU ASC
+);
+
+create index BTE_VCFFIF_I on BTE_VALCFU (
+   VCF_CODFIFU ASC
+);
+
+alter table BTE_CAMFUE
+   add constraint BTE_CFUFUE_FK foreign key (CFU_CODFUE)
+      references BTE_FUEDAT (FUE_CODIGO);
+
+alter table BTE_FILFUE
+   add constraint BTE_FIFUFUE_FK foreign key (FIF_CODFUE)
+      references BTE_FUEDAT (FUE_CODIGO);
+
+alter table BTE_FUEDAT
+   add constraint BTE_FUETAP_FK foreign key (FUE_IDEPRO)
+      references BTE_PROAPL (TAP_IDEPRO);
+
+alter table BTE_VALCFU
+   add constraint BTE_VCFCFU_FK foreign key (VCF_CODCFU)
+      references BTE_CAMFUE (CFU_CODIGO);
+
+alter table BTE_VALCFU
+   add constraint BTE_VCFFIFU_FK foreign key (VCF_CODFIFU)
+      references BTE_FILFUE (FIF_CODIGO);
+
