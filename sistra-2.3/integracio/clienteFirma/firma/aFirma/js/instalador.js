d96d94fc118480ac338e79f5eef12fd2f11f931c
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-03-13 14:29:35 +0000

    mid: 0000350 msg: Actualizacion cliente firma 3.3.1
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@919 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- sistra-2.3/integracio/clienteFirma/firma/aFirma/js/instalador.js
+++ sistra-2.3/integracio/clienteFirma/firma/aFirma/js/instalador.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 [Gobierno de Espana]
+/* Copyright (C) 2013 [Gobierno de Espana]
  * This file is part of "Cliente @Firma".
  * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
  *   - the GNU General Public License as published by the Free Software Foundation; 
@@ -10,23 +10,19 @@
 
 /**
  *
- * Version: 2.2.2
+ * Version: 4.0.0
  *
- * Depende de deployJava.js y de constantes.js [opcional].
+ * Dependencia opcional de constantes.js
  *
- * Si se ha definido baseDownloadURL se usa como URL base para la descarga de los instalables.
+ * Si se ha definido baseDownloadURL en constantes.js se usa como URL base para la descarga de los instalables.
  *
- * cargarAppletFirma(build):
- *      Carga el applet de firma en la variable "clienteFirma". Si el applet detecta que requiere alguna dependencia que no esta
- *		en el sistema del usuario, la instalara. Recibe como parametro la construccion del Cliente que se desea cargar "LITE", "MEDIA"
- *		o "COMPLETA".
+ * cargarAppletFirma():
+ *      Carga el applet de firma en la variable "clienteFirma".
  */
 
-var instalador;
-var instaladorCargado = false;
 var clienteFirma;
 
-function cargarAppletFirma(build, jnlp)
+function cargarAppletFirma(build)
 {
 	/* Si ya esta cargado, no continuamos con el proceso */
 	if (clienteFirma != undefined) {
@@ -35,30 +31,8 @@ function cargarAppletFirma(build, jnlp)
 	
 	/* Definimos las contruccion que se va a utilizar */
 	var confBuild = configureBuild(build);
-
-	/* Comprobamos si se desea la carga JNLP (opcion por defecto) */
-	var loadByJnlp = jnlp == undefined || jnlp == null || jnlp != false;
-	
-	/* Con Google Chrome nunca cargaremos el Bootloader, porque no soportaria 2 applet en la misma pagina si la JVM no esta actualizada. */
-	if (deployJava.browserName2 != 'Chrome') {	
-	
-		/* Cargaremos el Bootloader en aquellos casos en los que:
-		 *    - Tengamos Java 5.
-		 *    - Tengamos Java6u28 o inferior de 64 bits en Windows.
-		 *    - Tengamos Java7u0 o inferior de 64 bits en Windows.
-		 */
-		if ((!deployJava.versionCheck('1.6') && !deployJava.versionCheck('1.7')) ||
-		    (isWindows() && is64BitsSystem() && (!deployJava.versionCheck('1.6.0_29+') || deployJava.versionCheck('1.7.0_00')))) {
-				instalar(confBuild, null, null);
-		}
-	}
-	
 	var jarArchive = confBuild + "_j6_afirma5_core.jar";
-	if (deployJava.versionCheck('1.6.0+') == false) {
-                jarArchive = confBuild + "_j5_afirma5_core.jar";
-				loadByJnlp = false;
-	}
-	
+		
 	var codeBase = base;
 	if (codeBase == undefined || codeBase == null) {
 		codeBase = '.';
@@ -68,46 +42,31 @@ function cargarAppletFirma(build, jnlp)
 	if (defaultLocale == undefined) {
 		defaultLocale = null;
 	}
-	
+
 	var attributes = {
 		id: 'firmaApplet',
+		name: 'Applet Cliente @firma (Gobierno de Espa\u00F1a)',
+		type: 'application/x-java-applet',
 	 	width: 1,
 		height: 1
 	};
-	var parameters;
 	
-	// En el caso de Linux, desactivamos el despliegue de JNLP para evitar los problemas derivados de IcedTea
-	if (loadByJnlp && !isLinux()) {
-		parameters = {
+	// Se configuran los parametrospara un despliegue tradicional del applet debido a los problema con
+	// JNLP en Mac OS X, Linux con IcedTea y algunos entornos Windows 7/8
+	var parameters = {
 			userAgent: window.navigator.userAgent,
 			appName: window.navigator.appName,
 			showExpiratedCertificates: showExpiratedCertificates,
 			showMozillaSmartCardWarning: showMozillaSmartCardWarning,
-			code: 'es.gob.afirma.applet.SignApplet.class',
-			archive: jarArchive,
-			codebase: codeBase,
-			locale: defaultLocale,
-			java_arguments: '-Djnlp.packEnabled=true -Xms512M -Xmx512M',
-			separate_jvm: true,
-			jnlp_href: confBuild + '_afirma.jnlp'
-		};
-	}
-	else {
-		parameters = {
-			userAgent: window.navigator.userAgent,
-			appName: window.navigator.appName,
-			showExpiratedCertificates: showExpiratedCertificates,
-			showMozillaSmartCardWarning: showMozillaSmartCardWarning,
-			code: 'es.gob.afirma.applet.SignApplet.class',
-			archive: jarArchive,
-			codebase: codeBase,
+			code: 'es.gob.afirma.applet.SignApplet',
+			archive: codeBase + "/" + jarArchive,
 			locale: defaultLocale,
 			java_arguments: '-Xms512M -Xmx512M',
+			codebase_lookup: false,
 			separate_jvm: true
-		};
-	}
-	
- 	deployJava.runApplet(attributes, parameters, '1.5');
+	};
+
+	loadApplet(attributes, parameters);
 
 	clienteFirma = document.getElementById("firmaApplet");
 	
@@ -123,82 +82,7 @@ function cargarAppletFirma(build, jnlp)
 			 */
 		}
 	}
-}
-
-function instalar(build, jsMethodName, jsMethodParams)
-{
-	// Definimos las contruccion que se va a utilizar
-	var confBuild = configureBuild(build);
-
-	if(instalador == undefined)
-	{
-		cargarAppletInstalador(confBuild);
-	}
-}
-
-function cargarAppletInstalador(confBuild)
-{
-	if(instalador == undefined)
-	{
-		/* Definicion de las constantes necesarias */
-		var codeBase = '.';
-		if (base != undefined) {
-			if(base.toLowerCase().substring(0, 7) != "file://" && 
-					base.toLowerCase().substring(0, 7) != "http://" &&
-					base.toLowerCase().substring(0, 8) != "https://") {
-				codeBase = './' + base;
-			}
-			else {
-				codeBase = base;
-			}
-		}
-		
-		// Si hay definida una URL desde la que descargar los instalables, la establecemos
-		var baseDownloadURLVar = '.';
-		if (baseDownloadURL != undefined) {
-			if(baseDownloadURL.toLowerCase().substring(0, 7) != "file://" && 
-					baseDownloadURL.toLowerCase().substring(0, 7) != "http://" &&
-					baseDownloadURL.toLowerCase().substring(0, 8) != "https://") {
-
-				var url = document.location.toString();
-				baseDownloadURLVar = url.substring(0, url.lastIndexOf("/")) + '/' + baseDownloadURL;
-			}
-			else {
-				baseDownloadURLVar = baseDownloadURL;
-			}
-		}
-		
-		var parameters = {
-					baseDownloadURL:baseDownloadURLVar,
-					installType:confBuild
-					};
-		var attributes = {
-					id:'instaladorApplet',
-					code:'es.gob.afirma.install.AfirmaBootLoader.class',	
-					archive:codeBase+'/afirmaBootLoader.jar',
-					width:1, height:1
-					};
-		var version = '1.5';
-		
-		try {
-			deployJava.runApplet(attributes, parameters, version);
-		} catch(e) {}
-
-		instalador = document.getElementById("instaladorApplet");
-		
-		for(var i=0; i<100; i++) {
-			try {
-				setTimeout("instalador != undefined", 100);
-				instaladorCargado = true;
-				break;
-			} catch(e) {
-				instaladorCargado = false;
-				// Capturamos la excepcion que se produciria si no se hubiese cargado aun el applet, aunque no se lanzaria
-				// una vez estuviese cargado aunque no iniciado
-			}
-		}
-	}
-}
+};
 
 /**
  * Si no se ha indicado una construccion por parametro, ni hay establecida una por defecto en "constantes.js", se instala la 'LITE'
@@ -217,26 +101,51 @@ function configureBuild(build)
 		confBuild = 'LITE';
 	}
 	return confBuild;
-}
+};
 
- function isWindows()
- {
-	return navigator.appVersion.indexOf("Win") != -1;
- }
+function loadApplet (attributes, parameters) {
 
-  function isLinux()
- {
-	return navigator.appVersion.indexOf("Linux") != -1 || navigator.appVersion.indexOf("X11") != -1;
- }
- 
-function is64BitsSystem()
-{
-	var is64BitBrowser = false;
-	if( window.navigator.cpuClass != null && window.navigator.cpuClass.toLowerCase() == "x64" ) {
-		is64BitBrowser = true;
+	// Segun sea Internet Explorer 7/8 o cualquier otro navegador, inicializamos de una forma u otra el applet
+	if (navigator.appVersion.toUpperCase().indexOf("MSIE 7.0") != -1 || navigator.appVersion.toUpperCase().indexOf("MSIE 8.0") != -1) {
+		
+		var appletTag = "<object classid='clsid:8AD9C840-044E-11D1-B3E9-00805F499D93' width='1' height='1' id='" + attributes["id"] + "'>";
+			
+		if (attributes != undefined && attributes != null) {
+			for (var attribute in attributes) {
+				appletTag += "<param name='" + attribute + "' value='" + attributes[attribute] + "' />";
+			}
+		}
+
+		if (parameters != undefined && parameters != null) {
+			for (var parameter in parameters) {
+				appletTag += "<param name='" + parameter + "' value='" + parameters[parameter] + "' />";
+			}
+		}
+
+		appletTag += "</object>";
+		document.write(appletTag);
+
 	}
-	if( window.navigator.platform.toLowerCase() == "win64" ) {
-		is64BitBrowser = true
+	else {
+		var embed = document.createElement("embed");
+
+		if (attributes != undefined && attributes != null) {
+			for (var attribute in attributes) {
+				var att = document.createAttribute(attribute);
+				var attValue = attributes[attribute];
+				att.value = attValue;
+				embed.setAttributeNode(att);
+			}
+		}
+
+		if (parameters != undefined && parameters != null) {
+			for (var parameter in parameters) {
+				var att = document.createAttribute(parameter);
+				att.value = parameters[parameter];
+				embed.setAttributeNode(att);
+			}
+		}
+
+		document.body.appendChild(embed);
 	}
-	return is64BitBrowser;
 }
