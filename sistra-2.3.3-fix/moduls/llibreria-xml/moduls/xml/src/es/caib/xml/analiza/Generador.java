88741a4c93ef82e36db52ff94a08f057611fbfbb
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-05-26 17:04:30 +0000

    mid: 0000370 msg: XML: bug tratamiento XML
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@957 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- sistra-2.3.3-fix/moduls/llibreria-xml/moduls/xml/src/es/caib/xml/analiza/Generador.java
+++ sistra-2.3.3-fix/moduls/llibreria-xml/moduls/xml/src/es/caib/xml/analiza/Generador.java
@@ -90,7 +90,10 @@ public class Generador {
 		Iterator itHojas = nodosHojas.iterator();
 		while (itHojas.hasNext()){
 			
-			Object o = itHojas.next();
+			// Object o = itHojas.next();
+			String xpath = (String) itHojas.next();
+			Object o = nodosHojas.get(xpath);
+			
 			
 			if (o instanceof List){
 				List lista = (List) o;
