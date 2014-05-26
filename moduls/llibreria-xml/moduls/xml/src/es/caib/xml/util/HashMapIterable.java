b504bb61d53ce8944cb348a33479649d1ad270ea
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-05-26 17:06:10 +0000

    mid: 0000370 msg: XML: bug tratamiento XML
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-2.3@958 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- moduls/llibreria-xml/moduls/xml/src/es/caib/xml/util/HashMapIterable.java
+++ moduls/llibreria-xml/moduls/xml/src/es/caib/xml/util/HashMapIterable.java
@@ -1,11 +1,8 @@
 package es.caib.xml.util;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.List;
+import java.util.LinkedHashMap;
 
 /** 
  * HashMap que ademas, implementa la interfaz Iterator para poder
@@ -14,52 +11,10 @@ import java.util.List;
  * @author mangel
  *
  */
-public class HashMapIterable extends HashMap implements Serializable {
-	List lista = new ArrayList ();	
-	 
-	public Object put (Object key, Object value) throws NullPointerException {
-		Object anterior = super.put (key, value);
-		
-		if (anterior != null){
-			lista.remove (anterior);
-		}
-		
-		lista.add (value);
-		
-		return anterior;
+public class HashMapIterable extends LinkedHashMap implements Serializable {
+
+	public Iterator iterator() {
+		return super.keySet().iterator();
 	}
-		
-	
-	public Object putAtIndex (Object key, Object value, int index){
-		if ( (index < 0) || (index > lista.size())){
-			throw new IndexOutOfBoundsException ();
-		}
-		
-		if ( (key == null) || (value == null) ){
-			throw new NullPointerException ();
-		}
-		
-		Object anterior = super.put (key, value);
-		
-		if (anterior != null){
-			lista.remove (anterior);
-		}
-		
-		lista.add (index, value);
-						
-		return anterior;						
-	}
-	
-	public Object remove (Object key) throws NullPointerException {
-		Object anterior = super.remove (key);
-		
-		if (anterior != null) lista.remove (anterior);
-		
-		return null;
-	}
-	
-	public Iterator iterator (){
-		return new IteradorHashMap (this);
-	}
-	
+
 }
