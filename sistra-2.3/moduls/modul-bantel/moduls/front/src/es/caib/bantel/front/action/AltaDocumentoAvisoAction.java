a91d0fae7b66a7cd5ab46ac2e7c9049a89cfa704
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-05-28 14:00:37 +0000

    mid: 0000371 msg: Bantelfront: modulo de expedientes, permitir anexar directamente PDF
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@962 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- sistra-2.3/moduls/modul-bantel/moduls/front/src/es/caib/bantel/front/action/AltaDocumentoAvisoAction.java
+++ sistra-2.3/moduls/modul-bantel/moduls/front/src/es/caib/bantel/front/action/AltaDocumentoAvisoAction.java
@@ -1,6 +1,5 @@
 package es.caib.bantel.front.action;
 
-import java.io.ByteArrayInputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -24,7 +23,6 @@ import es.caib.xml.ConstantesXML;
 import es.caib.xml.documentoExternoNotificacion.factoria.FactoriaObjetosXMLDocumentoExternoNotificacion;
 import es.caib.xml.documentoExternoNotificacion.factoria.ServicioDocumentoExternoNotificacionXML;
 import es.caib.xml.documentoExternoNotificacion.factoria.impl.DocumentoExternoNotificacion;
-import es.indra.util.pdf.UtilPDF;
 
 /**
  * @struts.action
@@ -162,10 +160,12 @@ public class AltaDocumentoAvisoAction extends BaseAction
 		
 		// Si es un PDF comprobamos que sea PDF/A		
 		if  ("PDF".equalsIgnoreCase(DocumentosUtil.getExtension(avisoForm.getDocumentoAnexoFichero().getFileName()))) {
+			/*
 			if (!UtilPDF.isPdfA( new ByteArrayInputStream(avisoForm.getDocumentoAnexoFichero().getFileData()))) {
 				Log.error("No es PDF/A");
 				return null;
 			}
+			*/
 			convertirPDF = false;
 		} else {
 			// Si no es PDF, miramos si se puede convertir
