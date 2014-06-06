035a7e7fe0dcdbf73d5f19755b1c09f70eec881c
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-06-06 14:27:33 +0000

    mid: 0000362 msg: Mobtratel: control de envíos SMS para poder facturar el coste de los mismos
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-2.3@966 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- moduls/modul-mobtratel/moduls/webservices/src/es/caib/mobtratel/ws/v2/model/ObjectFactory.java
+++ moduls/modul-mobtratel/moduls/webservices/src/es/caib/mobtratel/ws/v2/model/ObjectFactory.java
@@ -25,26 +25,19 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
+    private final static QName _Fault_QNAME = new QName("urn:es:caib:mobtratel:ws:v2:model:BackofficeFacade", "fault");
     private final static QName _MensajeEnvioCuentaEmisora_QNAME = new QName("", "cuentaEmisora");
     private final static QName _MensajeEnvioEmails_QNAME = new QName("", "emails");
     private final static QName _MensajeEnvioFechaCaducidad_QNAME = new QName("", "fechaCaducidad");
+    private final static QName _MensajeEnvioIdProcedimiento_QNAME = new QName("", "idProcedimiento");
     private final static QName _MensajeEnvioSmss_QNAME = new QName("", "smss");
     private final static QName _MensajeEnvioFechaProgramacionEnvio_QNAME = new QName("", "fechaProgramacionEnvio");
-    private final static QName _Fault_QNAME = new QName("urn:es:caib:mobtratel:ws:v2:model:BackofficeFacade", "fault");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: es.caib.mobtratel.ws.v2.model
      * 
      */
     public ObjectFactory() {
-    }
-
-    /**
-     * Create an instance of {@link MensajeEnvioEmail }
-     * 
-     */
-    public MensajeEnvioEmail createMensajeEnvioEmail() {
-        return new MensajeEnvioEmail();
     }
 
     /**
@@ -64,6 +57,14 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link EnviarMensaje }
+     * 
+     */
+    public EnviarMensaje createEnviarMensaje() {
+        return new EnviarMensaje();
+    }
+
+    /**
      * Create an instance of {@link MensajesEnvioEmail }
      * 
      */
@@ -80,14 +81,6 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link EnviarMensaje }
-     * 
-     */
-    public EnviarMensaje createEnviarMensaje() {
-        return new EnviarMensaje();
-    }
-
-    /**
      * Create an instance of {@link BackofficeFacadeException }
      * 
      */
@@ -96,11 +89,28 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link MensajeEnvioEmail }
+     * 
+     */
+    public MensajeEnvioEmail createMensajeEnvioEmail() {
+        return new MensajeEnvioEmail();
+    }
+
+    /**
      * Create an instance of {@link EnviarMensajeResponse }
      * 
      */
     public EnviarMensajeResponse createEnviarMensajeResponse() {
         return new EnviarMensajeResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BackofficeFacadeException }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "urn:es:caib:mobtratel:ws:v2:model:BackofficeFacade", name = "fault")
+    public JAXBElement<BackofficeFacadeException> createFault(BackofficeFacadeException value) {
+        return new JAXBElement<BackofficeFacadeException>(_Fault_QNAME, BackofficeFacadeException.class, null, value);
     }
 
     /**
@@ -131,6 +141,15 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "idProcedimiento", scope = MensajeEnvio.class)
+    public JAXBElement<String> createMensajeEnvioIdProcedimiento(String value) {
+        return new JAXBElement<String>(_MensajeEnvioIdProcedimiento_QNAME, String.class, MensajeEnvio.class, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link MensajesEnvioSMS }{@code >}}
      * 
      */
@@ -146,15 +165,6 @@ public class ObjectFactory {
     @XmlElementDecl(namespace = "", name = "fechaProgramacionEnvio", scope = MensajeEnvio.class)
     public JAXBElement<XMLGregorianCalendar> createMensajeEnvioFechaProgramacionEnvio(XMLGregorianCalendar value) {
         return new JAXBElement<XMLGregorianCalendar>(_MensajeEnvioFechaProgramacionEnvio_QNAME, XMLGregorianCalendar.class, MensajeEnvio.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link BackofficeFacadeException }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "urn:es:caib:mobtratel:ws:v2:model:BackofficeFacade", name = "fault")
-    public JAXBElement<BackofficeFacadeException> createFault(BackofficeFacadeException value) {
-        return new JAXBElement<BackofficeFacadeException>(_Fault_QNAME, BackofficeFacadeException.class, null, value);
     }
 
 }
