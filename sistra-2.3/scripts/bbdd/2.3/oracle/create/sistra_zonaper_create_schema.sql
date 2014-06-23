ac56248b952650503ae1747a730e1ffd0a7a1b5a
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-06-23 08:11:55 +0000

    Actualizacion Scripts BBDD
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@981 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- sistra-2.3/scripts/bbdd/2.3/oracle/create/sistra_zonaper_create_schema.sql
+++ sistra-2.3/scripts/bbdd/2.3/oracle/create/sistra_zonaper_create_schema.sql
@@ -1891,5 +1891,11 @@ comment on column ZPE_DPEBCK.DPB_DLGFIP is
 'NIFS QUE QUEDAN POR FIRMAR EL DOCUMENTO SEPARADOS POR #';
 
  
+-- V 2.3.4
+-- CONTROL SMS
+ALTER table ZPE_TRAPER  ADD TPE_IDEPRO VARCHAR2(100);
+comment on column ZPE_TRAPER.TPE_IDEPRO is 'ID PROCEDIMIENTO';
 
-
+-- PLAZO VBLE NOTIF
+alter table ZPE_NOTTEL add NOT_DIAPLZ           NUMBER(3);
+comment on column ZPE_NOTTEL.NOT_DIAPLZ is 'En caso de que se establezca un plazo distinto a 10 dias';
