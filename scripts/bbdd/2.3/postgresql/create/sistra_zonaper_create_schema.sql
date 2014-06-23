858d259f67447eaf1860c0798cbbdc7571ead6df
anadal-ibit <anadal-ibit@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-06-23 09:13:17 +0000

    mid: 0000085 msg: Tasca Continua d'Actualització d'Scripts de BBDD (Ver 2.3.4)
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-2.3@985 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- scripts/bbdd/2.3/postgresql/create/sistra_zonaper_create_schema.sql
+++ scripts/bbdd/2.3/postgresql/create/sistra_zonaper_create_schema.sql
@@ -1895,4 +1895,12 @@ comment on column ZPE_PREBCK.PRB_ALTEMA is 'EMAIL PARA GENERAR ALERTAS';
 comment on column ZPE_PREBCK.PRB_ALTSMS is 'SMS PARA GENERAR ALERTAS';
 comment on column ZPE_PREBCK.PRB_ALTFEC is 'FECHA ULTIMA ALERTA';
 
+-- UPDATE FROM 2.3.3 TO 2.3.4
 
+-- CONTROL SMS
+ALTER table ZPE_TRAPER  ADD TPE_IDEPRO VARCHAR(100);
+comment on column ZPE_TRAPER.TPE_IDEPRO is 'ID PROCEDIMIENTO';
+
+-- PLAZO VBLE NOTIF
+alter table ZPE_NOTTEL add NOT_DIAPLZ           BIGINT;
+comment on column ZPE_NOTTEL.NOT_DIAPLZ is 'En caso de que se establezca un plazo distinto a 10 dias';
