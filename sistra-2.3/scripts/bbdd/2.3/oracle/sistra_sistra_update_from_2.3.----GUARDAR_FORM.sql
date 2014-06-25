c66839ae7e2ae00a16edc75726e9ef96bd2838a8
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2014-06-25 07:40:27 +0000

    mid: 000152 msg: Tasca Continua d'Actualització d'Scripts de BBDD
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches@986 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- sistra-2.3/scripts/bbdd/2.3/oracle/sistra_sistra_update_from_2.3.----GUARDAR_FORM.sql
+++
@@ -1,4 +0,0 @@
--- GUARDAR FORMULARIO
-ALTER table STR_DOCNIV  ADD DNV_FORGUA VARCHAR2(1) default 'N' not null;
-
-comment on column STR_DOCNIV.DNV_FORGUA is 'Para Formulario: indica si se permite guardar sin terminar';
