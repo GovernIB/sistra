bd839eff682ea3c0b3c4f38d5a31010a40a3f869
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2015-04-17 14:46:37 +0000

    Modulo consola
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-3.0@1133 5878cf19-a0df-4652-bd9f-a806d42fd1e1

---
+++ moduls/modul-consola/moduls/back/src/es/caib/consola/controller/gestor/wmodal/renderer/AnexosListitemRenderer.java
@@ -0,0 +1,52 @@
+package es.caib.consola.controller.gestor.wmodal.renderer;
+
+import org.zkoss.zk.ui.sys.ComponentsCtrl;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.ListitemRenderer;
+
+import es.caib.sistra.model.Documento;
+import es.caib.sistra.model.TraDocumento;
+
+/**
+ * Renderer lista anexos version tramite.
+ */
+public final class AnexosListitemRenderer implements ListitemRenderer {
+
+    /**
+     * Atributo idioma.
+     */
+    private final String idioma;
+
+    /**
+     * Instancia un nuevo formularios listitem renderer de
+     * FormulariosListitemRenderer.
+     * 
+     * @param pIdioma
+     *            Parametro idioma
+     */
+    public AnexosListitemRenderer(final String pIdioma) {
+        idioma = pIdioma;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.zkoss.zul.ListitemRenderer#render(org.zkoss.zul.Listitem,
+     * java.lang.Object)
+     */   
+    public void render(final Listitem item, final Object data) {
+
+    	final Documento ft = (Documento) data;
+
+    	item.setValue(data);
+    	item.setAttribute("data", data);
+
+        item.appendChild(new Listcell(ft.getIdentificador()));
+        item.appendChild(new Listcell( ((TraDocumento) ft.getTraduccion(idioma)).getDescripcion()));
+        
+        
+        
+        ComponentsCtrl.applyForward(item,"onDoubleClick=onDoubleClick$itemAnexo");
+    }
+}
