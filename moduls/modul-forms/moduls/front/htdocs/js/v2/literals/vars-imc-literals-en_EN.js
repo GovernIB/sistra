523bb628ac54eece81ddf67e1eea00861dd387dc
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2015-07-15 11:20:30 +0000

    Adecuacion literales
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-3.0@1186 5878cf19-a0df-4652-bd9f-a806d42fd1e1

---
+++ moduls/modul-forms/moduls/front/htdocs/js/v2/literals/vars-imc-literals-en_EN.js
@@ -0,0 +1,10 @@
+﻿// VARS
+
+var txtAjuda = "Help",
+	txtActivada = "enabled",
+	txtDesctivada = "disabled",
+	txtActiva = "Enable",
+	txtDesactiva = "Disable",
+	txtEnviantDades = "Sending data",
+	txtEspere = "Wait a moment, please";
+
