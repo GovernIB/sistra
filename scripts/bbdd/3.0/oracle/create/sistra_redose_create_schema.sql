1fcba54cf42a66fe98e3562610fc1092d2a7af09
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2015-10-15 11:14:05 +0000

    mid:#12 msg: Incluir CSV en Redose
    git-svn-id: https://svn.code.sf.net/p/sistra/code/sistra-core/branches/sistra-3.0@1242 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- scripts/bbdd/3.0/oracle/create/sistra_redose_create_schema.sql
+++ scripts/bbdd/3.0/oracle/create/sistra_redose_create_schema.sql
@@ -612,3 +612,30 @@ create index RDS_USODOC_IDX on RDS_USOS (
    USO_CODTIU ASC,
    USO_REF ASC
 );
+
+-- V3.0.2
+alter table RDS_DOCUM add DOC_CSV VARCHAR2(50);
+comment on column RDS_DOCUM.DOC_CSV is 'CSV';
+
+create unique index RDS_DOC_AK on RDS_DOCUM (
+   DOC_CSV ASC
+);
+
+create table RDS_TABCSV  (
+   TCS_CODIGO           VARCHAR2(1)                     not null,
+   TCS_CLAVES           BLOB                            not null
+);
+
+comment on table RDS_TABCSV is
+'TABLA TRANSFORMACION CSV';
+
+comment on column RDS_TABCSV.TCS_CODIGO is
+'CODIGO';
+
+comment on column RDS_TABCSV.TCS_CLAVES is
+'CLAVES';
+
+alter table RDS_TABCSV
+   add constraint RDS_TCS_PK primary key (TCS_CODIGO);
+
+
