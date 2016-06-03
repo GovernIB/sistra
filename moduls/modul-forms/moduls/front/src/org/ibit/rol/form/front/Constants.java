13c139857564555f8ddcb43bdc94a7ca924693d8
rsanz-indra <rsanz@indra.es>
2016-06-03 14:21:29 +0200

    Modificacion contextos Sistra #402

--- moduls/modul-forms/moduls/front/src/org/ibit/rol/form/front/Constants.java
+++ moduls/modul-forms/moduls/front/src/org/ibit/rol/form/front/Constants.java
@@ -11,22 +11,22 @@ public class Constants implements Serializable {
      * Atributo de contexto donde se guarda el lenguaje por defecto.
      */
     public static final String DEFAULT_LANG_KEY = "org.ibit.rol.form.front.DEFAULT_LANG";
-    
+
     /**
      * Atributo de contexto donde se guarda la info del organismo
      */
     public static final String ORGANISMO_INFO_KEY = "org.ibit.rol.form.front.infoOrganismo";
 
-    
+
     public static final String MOSTRAR_EN_IFRAME = "mostrarSistraEnIframe";
-    
-    
+
+
     /**
     * Atributo de sesion donde se guarda si la ayuda esta activada
     */
    public static final String AYUDA_ACTIVADA_KEY = "org.ibit.rol.form.front.ayudaActivada";
-   
+
    /** Contexto raiz sistra */
-	public static final String CONTEXTO_RAIZ = "es.caib.sistra.contextRoot";
-    
+	public static final String CONTEXTO_RAIZ = "es.caib.sistra.contextRootFront";
+
 }
