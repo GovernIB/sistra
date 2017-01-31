06c52fda80b693c60202cb098d45e3bc4c87d1cb
amaciam <amaciam@AMACIAM01W7.indra.es>
2017-01-31 20:06:59 +0100

    #463 Millora dels desplegables que mostren els òrgans de registre

---
+++ moduls/modul-bantel/moduls/back/src/es/caib/bantel/back/action/tramite/NodoUnidadAdministrativa.java
@@ -0,0 +1,93 @@
+package es.caib.bantel.back.action.tramite;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NodoUnidadAdministrativa implements Serializable
+{
+	
+	
+	private boolean folder;
+	private String id;
+	private String descripcion;
+	private String parentId;
+	private String action;	
+	private Map parameters = new HashMap();	
+	private String descripcionLink;
+	private String tipo="INDEFINIDO";
+	
+	
+	public NodoUnidadAdministrativa()
+	{
+		super();
+	}
+	
+	
+	public String getDescripcion()
+	{
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion)
+	{
+		this.descripcion = descripcion;
+	}
+	public boolean isFolder()
+	{
+		return folder;
+	}
+	public void setFolder(boolean folder)
+	{
+		this.folder = folder;
+	}
+	public String getId()
+	{
+		return id;
+	}
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+	public String getParentId()
+	{
+		return parentId;
+	}
+	public void setParentId(String parentId)
+	{
+		this.parentId = parentId;
+	}
+	public String getAction()
+	{
+		return action;
+	}
+	public void setAction(String action)
+	{
+		this.action = action;
+	}
+
+	public String getDescripcionLink()
+	{
+		return descripcionLink;
+	}
+
+	public void setDescripcionLink(String descripcionLink)
+	{
+		this.descripcionLink = descripcionLink;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Map getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Map parameters) {
+		this.parameters = parameters;
+	}	
+}
