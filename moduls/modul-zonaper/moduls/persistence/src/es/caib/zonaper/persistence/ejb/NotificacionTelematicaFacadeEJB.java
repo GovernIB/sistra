d309a1fbc4257587188fa08af214ff37b48f1f14
rsanz-indra <rsanz@indra.es>
2017-07-13 12:48:44 +0200

    #466 Pasar content type al plugin para firmaweb

--- moduls/modul-zonaper/moduls/persistence/src/es/caib/zonaper/persistence/ejb/NotificacionTelematicaFacadeEJB.java
+++ moduls/modul-zonaper/moduls/persistence/src/es/caib/zonaper/persistence/ejb/NotificacionTelematicaFacadeEJB.java
@@ -846,7 +846,7 @@ public abstract class NotificacionTelematicaFacadeEJB extends HibernateEJB {
 	private Map paramsPluginFirma(PluginFirmaIntf plgFirma)
 			throws Exception {
 		Map params = new HashMap();
-		if (plgFirma.getProveedor().equals(PluginFirmaIntf.PROVEEDOR_CAIB)) {
+		if (plgFirma.getProveedor().equals(PluginFirmaIntf.PROVEEDOR_FIRMAWEB)) {
 			// Establecemos content type correspondiente
 			params.put(	FirmaUtil.CAIB_PARAMETER_CONTENT_TYPE,
 						FirmaUtil.obtenerContentTypeCAIB(FirmaUtil.CAIB_ACUSE_NOTIFICACIO_CONTENT_TYPE));
