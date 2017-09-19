107d43b30f55790be2dc997ef1e887f97b2023f5
Indra <caibenred@indra.es>
2017-09-19 16:35:25 +0200

    #499 Mejora de tratamiento en certificados de representación para la recuperación de datos del representante

---
+++ moduls/modul-sistra/moduls/model/src/es/caib/sistra/model/DatosRepresentanteCertificado.java
@@ -0,0 +1,41 @@
+package es.caib.sistra.model;
+
+import java.io.Serializable;
+
+/**
+ * Datos del representante provenientes del certificado digital.
+ * @author rsanz
+ *
+ */
+public class DatosRepresentanteCertificado implements Serializable {
+	
+	private String nif;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    
+	public String getNif() {
+		return nif;
+	}
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+}
