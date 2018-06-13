6e9580562696cf0cbb7bb5ea1dbfa53dddbb9eb6
amaciam-indra <amaciam@indra.es>
2018-06-13 10:29:59 +0200

    #523 Missatge info a Les Meves Gestions

--- moduls/modul-zonaper/moduls/front/src/es/caib/zonaper/front/controller/MenuAutenticadoController.java
+++ moduls/modul-zonaper/moduls/front/src/es/caib/zonaper/front/controller/MenuAutenticadoController.java
@@ -1,5 +1,8 @@
 package es.caib.zonaper.front.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,11 +19,22 @@ public class MenuAutenticadoController extends BaseController
 			HttpServletRequest request, HttpServletResponse response,
 			ServletContext servletContext) throws Exception
 	{
+		Map infoNotificaciones = new HashMap();
 		
 		String notExternas = StringUtils.defaultString(DelegateUtil.getConfiguracionDelegate().obtenerConfiguracion().getProperty("notificaciones.enlaceExterno"), "");
+		String infoNotNoSistra = StringUtils.defaultString(DelegateUtil.getConfiguracionDelegate().obtenerConfiguracion().getProperty("notificaciones.noSistra.info.es"), "");
+		
+		infoNotificaciones.put("es", infoNotNoSistra);
+		
+		infoNotNoSistra = StringUtils.defaultString(DelegateUtil.getConfiguracionDelegate().obtenerConfiguracion().getProperty("notificaciones.noSistra.info.ca"), "");
+		infoNotificaciones.put("ca", infoNotNoSistra);
+		
+		infoNotNoSistra = StringUtils.defaultString(DelegateUtil.getConfiguracionDelegate().obtenerConfiguracion().getProperty("notificaciones.noSistra.info.en"), "");
+		infoNotificaciones.put("en", infoNotNoSistra);
 		
 		request.setAttribute("habilitarAlertas", Boolean.toString(ZonapersFrontRequestHelper.isHabilitarApartadoAlertas(request)));
 		request.setAttribute("notificacionesExternas", notExternas);
+		request.setAttribute("infoNotificacionsNoSistra", infoNotificaciones);
 		
 	}
 	
