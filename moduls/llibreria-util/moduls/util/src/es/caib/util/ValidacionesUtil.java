faff89426b8b105fa4e81b4561a64ca12b86589e
dmadronal-indra <dmadronal-indra@indra.es>
2019-08-05 19:50:26 +0200

    #535 ValidacionesUtil: incorporación de funciones de validación de código SWIFT

--- moduls/llibreria-util/moduls/util/src/es/caib/util/ValidacionesUtil.java
+++ moduls/llibreria-util/moduls/util/src/es/caib/util/ValidacionesUtil.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 
 public class ValidacionesUtil
 {
@@ -993,5 +994,69 @@ public class ValidacionesUtil
         return true;
     }
 
+	/**
+	 * Valida el código SWIFT de identificación del banco
+	 * No admite ningún tipo de separador, tan sólo el código SWIFT
+	 * @param codigo Código SWIFT
+	 * @return
+	 */
+	public static boolean esNumeroSwiftValido(final String codigo) {
+		boolean valido;
+		if (codigo == null || (codigo.length() != 8 && codigo.length() != 11)) {
+			valido = false;
+		} else {
+			final String banco = codigo.substring(0, 4);
+			final String pais = codigo.substring(4, 6);
+			final String localidad = codigo.substring(6, 8);
+			String sucursal = null;
+			if (codigo.length() == 11) {
+				sucursal = codigo.substring(8, 11);
+			}
+			valido = esNumeroSwiftValido(banco, pais, localidad, sucursal);
+		}
+		return valido;
+	}
+	
+	/**
+	 * Valida un código SWIFT a partir del código del banco, el país, la localidad y la sucursal
+	 * @param banco     Código Banco
+	 * @param pais      Código pais
+	 * @param localidad Código localidad
+	 * @param sucursal  Código sucursal (no obligatorio)
+	 * @return
+	 */
+	public static boolean esNumeroSwiftValido(final String banco, final String pais, final String localidad,
+			final String sucursal) {
+		final boolean codigoBancoCorrecto = banco != null && banco.length() == 4 && StringUtils.isAllUpperCase(banco);
+		final boolean codigoPaisCorrecto = pais != null && pais.length() == 2 && StringUtils.isAllUpperCase(pais);
+		final boolean codigoLocalidadCorrecto = localidad != null && localidad.length() == 2
+				&& (StringUtils.isNumeric(localidad) || StringUtils.isAllUpperCase(localidad));
+		final boolean codigoSucursalCorrecto = esNumeroSwiftValidoSucursal(sucursal);
 
+		return codigoBancoCorrecto && codigoPaisCorrecto && codigoLocalidadCorrecto && codigoSucursalCorrecto;
+	}
+	
+	/**
+	 * Comprueba si el número de sucursal es válido (es opcional este dato). 
+	 * Tienen que ser o una cadena de letras mayúsculas o numérica (mezclado).
+	 *
+	 * @param sucursal
+	 * @return
+	 */
+	private static boolean esNumeroSwiftValidoSucursal(final String sucursal) {
+		boolean validoSucursal;
+		if (sucursal == null) {
+			validoSucursal = true;
+		} else {
+			validoSucursal = true;
+			for (final char caracter : sucursal.toCharArray()) {
+				if (!StringUtils.isAllUpperCase(String.valueOf(caracter))
+						&& !StringUtils.isNumeric(String.valueOf(caracter))) {
+					validoSucursal = false;
+					break;
+				}
+			}
+		}
+		return validoSucursal;
+	}	
 }
