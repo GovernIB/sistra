e2a97e2cb279deb177e9ad80bf207d215249b88c
gcorullon <gcorullon@GCORULLON10.indra.es>
2020-05-19 02:14:52 +0200

    Adaptación de script PostgreSQL

--- scripts/bbdd/3.5/postgresql/sistra_bantel_update_from_3.5.14_to_3.5.15.sql
+++ scripts/bbdd/3.5/postgresql/sistra_bantel_update_from_3.5.14_to_3.5.15.sql
@@ -1,4 +1,4 @@
-alter table BTE_PROAPL  add   TAP_IDESIA           NUMBER(20);
+alter table BTE_PROAPL  add   TAP_IDESIA           bigint;
 
 comment on column BTE_PROAPL.TAP_IDESIA is
 'IDENTIFICADOR SIA';
